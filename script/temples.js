74c47e4130ef6c70eb7f3f94392beac4eb30deee
mayzabar26 <mayzabar26@gmail.com>
2024-06-22 12:48:11 -0300

    Temple Album assignment

--- script/temples.js
+++ script/temples.js
@@ -1,11 +1,13 @@
 // Store the selected elements that we are going to use. 
-const mainnav = document.querySelector(".nav-bar")
-const hambutton = document.querySelector("#menu");
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.navLinks');
+const title = document.querySelector('.title');
 
 // Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
-hambutton.addEventListener('click', () => {
-	mainnav.classList.toggle("show");
-	hambutton.classList.toggle("show");
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+    title.style.display = navLinks.classList.contains('open') ? 'none' : 'block';
+    hamburger.textContent = navLinks.classList.contains('open') ? '✕' : '☰';
 });
 
 
