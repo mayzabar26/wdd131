b9d5e5a2e81d660245b583778941525f11710967
mayzabar26 <mayzabar26@gmail.com>
2024-06-29 21:17:58 -0300

    final details

--- place-scripts/place.js
+++ place-scripts/place.js
@@ -1,6 +1,12 @@
-//function for last modified element
+//creating function for current year - footer
+function getCurrentYear() {
+    const currentYear = new Date().getFullYear();
+    document.getElementById("currentyear").textContent = currentYear;
+}
+
+//creating function for last modified
 function getLastModified () {
-    const lasModified = document.lastModified;
+    const lastModified = document.lastModified;
     document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`;
 }
 
